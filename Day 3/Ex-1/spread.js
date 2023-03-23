91818793ba328dc5a4cff43686b79ee5cd8f0cc0
anzar000 <peerzada.anzar@gmail.com>
2023-03-23 10:44:24 +0530

    more examples

--- Day 3/Ex-1/spread.js
+++ Day 3/Ex-1/spread.js
@@ -6,3 +6,12 @@ let arr2 = [6, 7, 8, 9, 10];
 let arr = [...arr1, ...arr2];
 
 console.log(arr);
+
+// Another Example
+
+let arr3 = [1, 2, 3, 'abc', 5];
+let arr4 = [6, 7, 8, 9, 10];
+
+let arr5 = [...arr3, 4, ...arr4];
+
+console.log(arr5);
--- Day 3/Ex-2/rest.js
+++ Day 3/Ex-2/rest.js
@@ -1,19 +1,60 @@
 // Demonstration of rest operator
 
-function exa(a,b){
-    return a+b;
+function exa(a, b) {
+    return a + b;
 }
 
-console.log(exa(1,2))
-console.log(exa(1,2,3,4,5,6,7,8,9,10))
+console.log(exa(1, 2))
+console.log(exa(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 
-function newexa(...input){
+function newexa(...input) {
     let sum = 0;
-    for(let i of input){
+    for (let i of input) {
         sum += i;
     }
     return sum;
 }
 
-console.log(newexa(1,2))
-console.log(newexa(1,2,3,4,5,6,7,8,9,10))
+console.log(newexa(1, 2))
+console.log(newexa(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+// Another Example
+console.log();
+function mul(...input) {
+    let mul = 1;
+    for (let i of input) {
+        mul *= i;
+    }
+    return mul;
+}
+
+console.log(mul(1, 2))
+console.log(mul(1, 2, 3))
+
+// Another Example
+console.log();
+function myFunction(a, b, ...others) {
+    console.log("a", a);
+    console.log("b", b);
+    console.log("others", others);
+}
+
+myFunction("one", "two", "three", "four", "five", "six");
+
+// Another Example
+console.log();
+function newexa1(a,b, ...input) {
+    console.log("a", a);
+    console.log("b", b);
+    console.log("input", input);
+
+    let sum = 0;
+    for (let i of input) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(newexa1(1, 2))
+console.log(newexa1(1, 2,3))
+console.log(newexa1(1,2,3,4))
