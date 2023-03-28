b95ba3632749e206a22714e259caff5cd25558a7
anzar000 <peerzada.anzar@gmail.com>
2023-03-28 16:53:38 +0530

    TS-L11

--- TS Labs/Labs 1-8/app.js
+++ TS Labs/Labs 1-8/app.js
@@ -33,3 +33,60 @@ var foodItems = ["Sandwich", "Pizza", "Pasta", "Salad", "Pancake", "Rice", "Icec
 foodItems.forEach(function (item, index) {
     console.log("".concat(index + 1, ". ").concat(item));
 });
+// Lab - 10
+var RestaurantType;
+(function (RestaurantType) {
+    RestaurantType[RestaurantType["Cafe"] = 0] = "Cafe";
+    RestaurantType[RestaurantType["Restaurant"] = 1] = "Restaurant";
+    RestaurantType[RestaurantType["FastFood"] = 2] = "FastFood";
+    RestaurantType[RestaurantType["Bakery"] = 3] = "Bakery";
+})(RestaurantType || (RestaurantType = {}));
+// Add a new restaurant using the contract defined.
+var Subway = {
+    name: "Subway",
+    address: "#123/A Avenue",
+    phone: 9878987878,
+    type: RestaurantType[RestaurantType.Cafe],
+};
+// Function to view the details of the restaurant
+var RestaurantDetails = function (restaurant) {
+    console.log("Name: ".concat(restaurant.name, " Address: ").concat(restaurant.address, " Phone: ").concat(restaurant.phone, " Type: ").concat(restaurant.type));
+};
+RestaurantDetails(Subway);
+// Lab - 11
+/*
+Using the contract defined in the previous lab for restaurant information, we now have
+a request to add two more restaurants MTR and Paradise which are of type Fast Food
+Corner and Bistros respectively.
+Tasks
+1 . Create a list to store the information of MTR and Paradise restaurants using the
+interface defined.
+2. Push the previous Subway Restaurant information in this list.
+3. Create a function to iterate through the list of restaurants using forEach and display
+the details of each restaurant with the Restaurant ID (index+l )
+*/
+// Create a list to store the information of MTR and Paradise restaurants using the interface defined.
+var resturantList = [
+    {
+        name: "MTR",
+        address: "#123/A Avenue",
+        phone: 9878987878,
+        type: RestaurantType[RestaurantType.FastFood],
+    },
+    {
+        name: "Paradise",
+        address: "#123/A Avenue",
+        phone: 9878987878,
+        type: RestaurantType[RestaurantType.Restaurant],
+    },
+];
+// Push the previous Subway Restaurant information in this list.
+resturantList.push(Subway);
+// Create a function to iterate through the list of restaurants using forEach and display the details of each restaurant with the Restaurant ID (index+l )
+var GetResturantList = function () {
+    resturantList.forEach(function (item, index) {
+        console.log("Restaurant ID: ".concat(index + 1));
+        RestaurantDetails(item);
+    });
+};
+GetResturantList();
--- TS Labs/Labs 1-8/app.ts
+++ TS Labs/Labs 1-8/app.ts
@@ -46,3 +46,83 @@ const foodItems = ["Sandwich", "Pizza", "Pasta", "Salad", "Pancake", "Rice", "Ic
 foodItems.forEach((item, index) => {
     console.log(`${index + 1}. ${item}`);
 });
+
+// Lab - 10
+
+enum RestaurantType {
+    "Cafe",
+    "Restaurant",
+    "FastFood",
+    "Bakery",
+}
+
+// Create an interface for Restaurant Info with Name(string), Address(string), Phone(number), Type(string) as the contract.
+interface RestaurantInfo {
+    name: string;
+    address: string;
+    phone: number;
+    type: string;
+}
+
+// Add a new restaurant using the contract defined.
+let Subway: RestaurantInfo = {
+    name: "Subway",
+    address: "#123/A Avenue",
+    phone: 9878987878,
+    type: RestaurantType[RestaurantType.Cafe],
+};
+
+// Function to view the details of the restaurant
+let RestaurantDetails = (restaurant) => {
+    console.log(`Name: ${restaurant.name} Address: ${restaurant.address} Phone: ${restaurant.phone} Type: ${restaurant.type}`);
+}
+
+RestaurantDetails(Subway);
+
+// Lab - 11
+/*
+Using the contract defined in the previous lab for restaurant information, we now have
+a request to add two more restaurants MTR and Paradise which are of type Fast Food
+Corner and Bistros respectively.
+Tasks
+1 . Create a list to store the information of MTR and Paradise restaurants using the
+interface defined.
+2. Push the previous Subway Restaurant information in this list.
+3. Create a function to iterate through the list of restaurants using forEach and display
+the details of each restaurant with the Restaurant ID (index+l )
+*/
+
+
+// Create a list to store the information of MTR and Paradise restaurants using the interface defined.
+
+let resturantList: RestaurantInfo[] = [
+    {
+        name: "MTR",
+        address: "#123/A Avenue",
+        phone: 9878987878,
+        type: RestaurantType[RestaurantType.FastFood],
+    },
+    {
+        name: "Paradise",
+        address: "#123/A Avenue",
+        phone: 9878987878,
+        type: RestaurantType[RestaurantType.Restaurant],
+    },
+];
+
+// Push the previous Subway Restaurant information in this list.
+
+resturantList.push(Subway);
+
+// Create a function to iterate through the list of restaurants using forEach and display the details of each restaurant with the Restaurant ID (index+l )
+
+let GetResturantList = () => {
+    resturantList.forEach((item, index) => {
+        console.log(`Restaurant ID: ${index + 1}`);
+        RestaurantDetails(item);
+    });
+}
+
+GetResturantList();
+
+
