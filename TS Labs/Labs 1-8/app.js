699cf26acd0fbdb9158837318f5375ac0dd9e5fc
anzar000 <peerzada.anzar@gmail.com>
2023-03-28 15:37:11 +0530

    Lab 2 - TS

---
+++ TS Labs/Labs 1-8/app.js
@@ -0,0 +1,8 @@
+// Lab - 1
+var message = "Welcome to Quick Food Restaurant";
+console.log(message);
+// Lab - 2
+var customerID = 11102;
+var customerName = "ABC";
+console.log("Customer ID: " + customerID);
+console.log("Customer Name: " + customerName);
---
+++ TS Labs/Labs 1-8/app.ts
@@ -0,0 +1,12 @@
+// Lab - 1
+var message: string = "Welcome to Quick Food Restaurant"
+
+console.log(message)
+
+// Lab - 2
+
+let customerID: number = 11102;
+let customerName: string = "ABC";
+
+console.log("Customer ID: " + customerID);
+console.log("Customer Name: " + customerName);
