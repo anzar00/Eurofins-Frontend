617e889fe7cf1ceadaa292a9a1cd84e0ad30df25
anzar000 <peerzada.anzar@gmail.com>
2023-03-28 15:49:21 +0530

    TS-L3

--- TS Labs/Labs 1-8/app.js
+++ TS Labs/Labs 1-8/app.js
@@ -6,3 +6,11 @@ var customerID = 11102;
 var customerName = "ABC";
 console.log("Customer ID: " + customerID);
 console.log("Customer Name: " + customerName);
+// Lab - 3
+function totalPrice(n1, n2) {
+    return n1 + n2;
+}
+var itemPrice = 100;
+var deliveryCharge = 50.80;
+var total = totalPrice(itemPrice, deliveryCharge);
+console.log("Total Price of the order is : Rs. ".concat(total));
--- TS Labs/Labs 1-8/app.ts
+++ TS Labs/Labs 1-8/app.ts
@@ -10,3 +10,15 @@ let customerName: string = "ABC";
 
 console.log("Customer ID: " + customerID);
 console.log("Customer Name: " + customerName);
+
+// Lab - 3
+
+function totalPrice(n1,n2) {
+    return n1 + n2;
+}
+
+const itemPrice = 100;
+const deliveryCharge = 50.80;
+const total = totalPrice(itemPrice, deliveryCharge);
+
+console.log(`Total Price of the order is : Rs. ${total}`);
