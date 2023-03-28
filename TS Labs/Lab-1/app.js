68b1b2327299d6e1784f128ab7671f1cb5040fc3
anzar000 <peerzada.anzar@gmail.com>
2023-03-29 02:19:06 +0530

    TS Labs done

--- TS Labs/Lab-1/app.js
+++
@@ -1,4 +0,0 @@
-//Write your typescript code here
-var message = "Welcome to Quick Food Restaurant";
-//print the message in console
-console.log(message);
--- TS Labs/Lab-1/app.ts
+++
@@ -1,6 +0,0 @@
-//Write your typescript code here
-var message:string = "Welcome to Quick Food Restaurant"
-
-//print the message in console
-
-console.log(message)
