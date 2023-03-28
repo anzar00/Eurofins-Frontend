813c81e9258abb5e2efdd645235371d0d3563e37
anzar000 <peerzada.anzar@gmail.com>
2023-03-28 10:07:39 +0530

    baisc

--- TypeScript/Example.js
+++ TypeScript/Example.js
@@ -1,4 +1,7 @@
+"use strict";
 // Learning TypeScript
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sum = void 0;
 // Variable Decleration
 var a = 10;
 // Strictly Typed Variable
@@ -10,6 +13,10 @@ console.log(c);
 var Name = "Anzar";
 var Age = 23;
 var Score = 100;
+var value = "Excellent";
+console.log(value);
+value = 100;
+console.log(value);
 console.log(Name);
 if (Score >= 35) {
     console.log("Pass");
@@ -17,3 +24,29 @@ if (Score >= 35) {
 else {
     console.log("Fail");
 }
+// Class Sum Example
+var sum = /** @class */ (function () {
+    function sum() {
+        // Declaring and Initializing Variables
+        this.num1 = 10;
+        this.num2 = 20;
+        this.sampleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        this.i = 0;
+    }
+    // Function to print Array
+    sum.prototype.printArray = function () {
+        for (this.i = 0; this.i < this.sampleArray.length; this.i++) {
+            {
+                console.log(this.sampleArray[this.i]);
+            }
+        }
+    };
+    // Function to print sum
+    sum.prototype.printSum = function () {
+        console.log(this.num1 + this.num2);
+    };
+    return sum;
+}());
+exports.sum = sum;
+var ClassSum = new sum();
+console.log(ClassSum.printSum());
--- TypeScript/Example.ts
+++ TypeScript/Example.ts
@@ -15,11 +15,44 @@ console.log(c);
 var Name: String = "Anzar";
 var Age: number = 23;
 var Score: number = 100;
+var value: any = "Excellent";
+
+console.log(value);
+
+value = 100;
+
+console.log(value);
 
 console.log(Name);
 
-if(Score >=35){
+if (Score >= 35) {
     console.log("Pass");
 } else {
     console.log("Fail");
 }
+// Class Sum Example
+export class sum {
+    // Declaring and Initializing Variables
+    num1: number = 10;
+    num2: number = 20;
+    sampleArray: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    i: number = 0;
+
+    // Function to print Array
+    printArray(): void {
+        for (this.i = 0; this.i < this.sampleArray.length; this.i++) {
+            {
+                console.log(this.sampleArray[this.i]);
+            }
+        }
+    }
+
+    // Function to print sum
+    printSum(): void {
+        console.log(this.num1 + this.num2);
+    }
+}
+
+var ClassSum = new sum();
+console.log(ClassSum.printSum());
+console.log(ClassSum.printArray());
