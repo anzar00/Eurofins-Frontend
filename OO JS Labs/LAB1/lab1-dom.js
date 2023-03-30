4f9dbd92690433b097c3e2901f3c0bbb8ba2bd1b
anzar000 <peerzada.anzar@gmail.com>
2023-03-30 12:49:37 +0530

    OO-1

---
+++ OO JS Labs/LAB1/lab1-dom.js
@@ -0,0 +1,10 @@
+//1. [DOM] Add a background color 'yellow' and add a bottom border to the header 'Fiery Restaurant': solid, 5px and black color
+
+var header = document.getElementById("headerTitle");
+header.style.backgroundColor = "yellow";
+header.style.borderBottom = "solid 5px #000";
+
+//Step1: Use 'getElementById' to get the header and store it in a variable. 
+//Note: Class name of header is 'headerTitle'.
+//var title = 'Write code here'
+
---
+++ OO JS Labs/LAB1/readme.md
@@ -0,0 +1,10 @@
+## OO JS Lab 1
+
+For making the application look good client wants you to add the background color and the bottom
+border for the header of the 'Fiery Restaurant' application.
+
+Tasks:
+1. Use 'getElementById' to get the header and store it in a variable. Note: Class name of header is
+'headerTitle'.
+2. Background color is set as 'Yellow'.
+3. Bottom border is set as solid, 5px and black color.
