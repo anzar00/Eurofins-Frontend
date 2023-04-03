399729eb29eff1151b5bd2f2e29251622f7d238a
anzar000 <peerzada.anzar@gmail.com>
2023-04-04 01:16:00 +0530

    LAB12

--- Angular Labs/SkillAssure-Fiery Restaurant/src/shared/pipes/employeeTitle.pipe.ts
+++ Angular Labs/SkillAssure-Fiery Restaurant/src/shared/pipes/employeeTitle.pipe.ts
@@ -7,9 +7,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class EmployeeTitlePipe implements PipeTransform{
     transform(value: string, gender:string): string{
         // Lab 12: Custom Pipes - Salutation
+        if(gender == "M"){
+            return "Mr. " + value;
+        }
+        else{
+            return "Ms. " + value;
+        }
 
-        
-        return "";
     }
 }
 
