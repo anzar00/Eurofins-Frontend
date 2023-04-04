431e00410878cbcbd16de06e24c9a8a17da77626
anzar000 <peerzada.anzar@gmail.com>
2023-04-04 14:21:32 +0530

    LAB14

--- Angular Labs/SkillAssure-Fiery Restaurant/src/app/Employee/employee.component.ts
+++ Angular Labs/SkillAssure-Fiery Restaurant/src/app/Employee/employee.component.ts
@@ -4,6 +4,7 @@ import { EmployeeService } from '../../core/services/EmployeeService';
 // import { error } from '@angular/compiler/src/util';
 // import { ThrowStmt } from '@angular/compiler';
 
+
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -13,21 +14,15 @@ import { EmployeeService } from '../../core/services/EmployeeService';
 export class EmployeeComponent implements OnInit {
 
   // Lab 14: Services - getEmployees()
-  employees: Employee[] = [
-    {EmployeeId: 1, Name: "John Doe", Gender: "M", DOB: new Date(1992, 1, 22), Area: "Whitefield", IsAvailable: true},
-    {EmployeeId: 2, Name: "Jane Doe", Gender: "F", DOB: new Date(1992, 1, 22), Area: "Whitefield", IsAvailable: true},
-    {EmployeeId: 3, Name: "John Smith", Gender: "M", DOB: new Date(1985, 7, 12), Area: "Marathahalli", IsAvailable: true},
-    {EmployeeId: 4, Name: "Emily Johnson", Gender: "F", DOB: new Date(1990, 4, 5), Area: "Koramangala", IsAvailable: false},
-    {EmployeeId: 5, Name: "David Lee", Gender: "M", DOB: new Date(1998, 11, 17), Area: "Indiranagar", IsAvailable: true},
-    {EmployeeId: 6, Name: "Megan Chen", Gender: "F", DOB: new Date(1995, 3, 25), Area: "Bellandur", IsAvailable: true},
-    {EmployeeId: 7, Name: "Daniel Kim", Gender: "M", DOB: new Date(1989, 8, 9), Area: "Jayanagar", IsAvailable: false},
-  ];
+  employees: Employee[] = [];
   constructor(private _employeeService: EmployeeService) {
-
+    
   }
 
   ngOnInit(): void {
 
+
+    this.employees = this._employeeService.getEmployees();
 
     // Lab 15: HTTP Service - RxJs
 
--- Angular Labs/SkillAssure-Fiery Restaurant/src/core/services/EmployeeService.ts
+++ Angular Labs/SkillAssure-Fiery Restaurant/src/core/services/EmployeeService.ts
@@ -1,14 +1,27 @@
+import { Injectable } from '@angular/core';
 import { Employee } from '../../shared/models/Employee';
 
 // Lab 14: Services - Injectable()
 
+@Injectable()
 export class EmployeeService {
-    
 
-    url:string = 'http://219.65.96.170:9101/api/employee';
-    
-    constructor(){}
-    
+    getEmployees(): Employee[] {
+        return [
+            { EmployeeId: 1, Name: "John Doe", Gender: "M", DOB: new Date(1992, 1, 22), Area: "Whitefield", IsAvailable: true },
+            { EmployeeId: 2, Name: "Jane Doe", Gender: "F", DOB: new Date(1992, 1, 22), Area: "Whitefield", IsAvailable: true },
+            { EmployeeId: 3, Name: "John Smith", Gender: "M", DOB: new Date(1985, 7, 12), Area: "Marathahalli", IsAvailable: true },
+            { EmployeeId: 4, Name: "Emily Johnson", Gender: "F", DOB: new Date(1990, 4, 5), Area: "Koramangala", IsAvailable: false },
+            { EmployeeId: 5, Name: "David Lee", Gender: "M", DOB: new Date(1998, 11, 17), Area: "Indiranagar", IsAvailable: true },
+            { EmployeeId: 6, Name: "Megan Chen", Gender: "F", DOB: new Date(1995, 3, 25), Area: "Bellandur", IsAvailable: true },
+            { EmployeeId: 7, Name: "Daniel Kim", Gender: "M", DOB: new Date(1989, 8, 9), Area: "Jayanagar", IsAvailable: false },
+        ]
+    }
+
+    url: string = 'http://219.65.96.170:9101/api/employee';
+
+    constructor() { }
+
     // Lab 15: HTTP Service - RxJs - http.get
 
 
